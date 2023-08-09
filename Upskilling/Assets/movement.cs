38b255e8e2a5f885d9877ab273bfa3be1b0a01d3
KyleRandomnes <kylejleg@gmail.com>
2023-08-09 12:49:43 +1200

    oh there goes gravity

--- Upskilling/Assets/movement.cs
+++ Upskilling/Assets/movement.cs
@@ -6,7 +6,8 @@ public class movement : MonoBehaviour
 
     void Start()
     {
-        rb.AddForce(0, 200, 500);
+        rb.useGravity = false;
+//        rb.AddForce(0, 200, 500);
     }
 
     // Update is called once per frame
