89530aace35357304044b668561bcd5501f71efe
KyleRandomnes <kylejleg@gmail.com>
2023-08-09 12:23:54 +1200

    no gravity

--- Upskilling/Assets/movement.cs
+++ Upskilling/Assets/movement.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
+    public Rigidbody rb;
+
     void Start()
     {
-        
+        rb.useGravity = false;
     }
 
     // Update is called once per frame
