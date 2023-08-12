1cb81cefc0fb49fb52346c0eab36efc686c51e0e
KyleRandomnes <kylejleg@gmail.com>
2023-08-12 17:00:24 +1200

    added left and right movement

--- Upskilling/Assets/movement.cs
+++ Upskilling/Assets/movement.cs
@@ -5,14 +5,20 @@ public class movement : MonoBehaviour
     public Rigidbody rb;
 
     public float forwardforce = 2000f;
-
-    void Start()
-    {
-    }
-
+    public float sidewaysforce = 500f;
     // Update is called once per frame
     void FixedUpdate()
     {
+        //applies a forward force
         rb.AddForce(0, 0, forwardforce * Time.deltaTime);
+        
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysforce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysforce * Time.deltaTime, 0, 0);
+        }
     }
 }
